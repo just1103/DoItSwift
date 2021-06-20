c5f84a354f58a9a31946ed4d8e74fcf7a9acf837
Kevin <hyojuson1103@gmail.com>
2021-06-20 21:25:56 +0900

    Edit2 12_TableViewController

--- 12_TableViewController/12_TableViewController/AddViewController.swift
+++ 12_TableViewController/12_TableViewController/AddViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class AddViewController: UIViewController {
+    
+    @IBOutlet var tfAddItem: UITextField!
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func btnAddItem(_ sender: UIButton) {
+    }
+    
     
 
     /*
--- 12_TableViewController/12_TableViewController/TableViewController.swift
+++ 12_TableViewController/12_TableViewController/TableViewController.swift
@@ -7,8 +7,14 @@
 
 import UIKit
 
+var items = ["책 구매", "철수와 약속", "스터디 준비하기"] // 외부변수 선언 (모든 Class에서 해당 변수를 사용 가능함) - 전역변수와 어떻게 다른거지?
+var itemsImageFile = ["cart.png","clock.png","pencil.png"] // 외부변수 선언
+
 class TableViewController: UITableViewController {
 
+    @IBOutlet var tvListView: UITableView! // cell 하단의 Table View (Prototype Content)와 연결
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,25 +27,30 @@ class TableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
+    // return the number of sections - 보통 Table 내에 Section이 1개이므로 return값을 1로 지정 // section이 뭐지?
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
+    // return the number of rows - Section당 Row 개수는 Item 개수이므로 return값을 items.count로 지정
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return items.count
     }
 
-    /*
+    // insert a cell for the given index path (어떤 Row에 배치할지) - 변수의 내용을 Cell에 적용하는 함수 (제공된 주석을 Uncomment하여 작성)
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) // withIdentifier에 storyboard에서 지정한 Cell Id 입력. *dequeue (Double-ended Queue /덱/) : 자료구조 형태. 양쪽에서 삽입/삭제가 가능한 특이한 큐. dequeueReusableCell 메서드 - recycles or creates the cell
 
         // Configure the cell...
-
+        
+        cell.textLabel?.text = items[(indexPath as NSIndexPath).row] // Cell의 textLabel에 items Array를 대입 (array1[index] 형태로 값에 접근)
+        cell.imageView?.image = UIImage(named: itemsImageFile[(indexPath as NSIndexPath).row]) // Cell의 imageView에 itemsImageFile Array를 대입
+        
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
@@ -49,7 +60,7 @@ class TableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
@@ -59,7 +70,7 @@ class TableViewController: UITableViewController {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
